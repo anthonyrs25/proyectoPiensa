23fc596de2ad6c672ba6b906bc97f3ba83e3b5b0
Anthony <afrodriguez@sudamericano.edu.ec>
2026-02-06 12:52:23 -0500

    Header

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonContent, IonFooter, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonButtons, IonHeader, IonToolbar, IonContent, IonFooter, IonButtons, IonButton, IonIcon],
 })
 export class HomePage {
   constructor() {}
+
+scrollTo(sectionId: string) {
+const el = document.getElementById(sectionId);
+el?.scrollIntoView({behavior:'smooth',block:'start'});
+}
+
+openThemeModal() {
+  console.log('Abrir modal de Tema');
+}
+
+openLanguageModal() {
+  console.log('Abrir modal de Idioma');
+}
+
+openLoginModal() {
+  console.log('Abrir modal de Login');
+}
 }
